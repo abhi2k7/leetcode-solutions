a6ebab4701cf23335c346f47d75b56ec9fcdbb1c
Abhishek M <mashalakarabhishek7@gmail.com>
2026-09-10 23:23:02 +0530

    Add solution for 7. Reverse Integer

---
+++ 7.reverse-integer.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0;
+
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (rev > INT_MAX || rev < INT_MIN)
+            return 0;
+
+        return (int)rev;
+    }
+};
+// @lc code=end
+
