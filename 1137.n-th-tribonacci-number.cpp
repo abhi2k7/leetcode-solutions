bf5925f25da5eb4f04eadd849763a9b06fe1950c
Abhishek M <mashalakarabhishek7@gmail.com>
2026-09-12 21:50:38 +0530

    Add solution for 1137. N Th Tribonacci Number

---
+++ 1137.n-th-tribonacci-number.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1137 lang=cpp
+ *
+ * [1137] N-th Tribonacci Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int tribonacci(int n) {
+        int a = 0,b = 1,c = 1;
+        if(n == 0){return 0;}
+        if(n == 1 || n == 2){return 1;}
+        for(int i = 3;i <= n;i++){
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+};
+// @lc code=end
+
